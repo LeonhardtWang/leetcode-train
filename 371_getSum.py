af0d40aad8172bf43710e79879574f3a8e85d821
Leo Wang <953744841@qq.com>
2019-02-10 23:48:13 +0800

    upgraded

--- 371_getSum.py
+++ 371_getSum.py
@@ -5,8 +5,9 @@ class Solution:
         :type b: int
         :rtype: int
         """
+        # python的int为64位，所以在64位上计算，但此法必须控制在32位内
         while b != 0:
             c = ((a & b) << 1 ) & 0xFFFFFFFF # 进位
             a = (a ^ b) & 0xFFFFFFFF # 无进位求和
             b = c
-        return a if a < 0X7fffffff else ~(a ^ 0xFFFFFFFF)
+        return a if a < 0X7FFFFFFF else ~(a ^ 0xFFFFFFFF)
