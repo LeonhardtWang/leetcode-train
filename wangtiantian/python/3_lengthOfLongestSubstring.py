88f58a4c5dcf57b4598775e719f0790877166a7d
LeonhardtWang <953744841@qq.com>
2022-11-17 22:03:20 +0800

    up

--- wangtiantian/python/3_lengthOfLongestSubstring.py
+++ wangtiantian/python/3_lengthOfLongestSubstring.py
@@ -17,3 +17,21 @@ class Solution:
                     break
             res = max(res, len(char_set))
         return res 
+
+
+# 滑动窗口
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) <= 1:
+            return len(s)
+        left = 0
+        right = 0
+        max_len = 0
+        while right < len(s):
+            curr_s = s[left:right+1]
+            max_len = max(max_len, len(curr_s))
+            while left < right + 1 and right + 1 < len(s) and s[right+1] in curr_s:
+                left += 1
+                curr_s = s[left:right+1]
+            right += 1
+        return max_len
